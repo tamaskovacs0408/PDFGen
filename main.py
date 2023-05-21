e8c370744d8e9a0af8a43fe9b0dc7b5a77db8daf
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-05-21 21:59:10 +0200

    Generating cell to pdf

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 from fpdf import FPDF
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+pdf.add_page()
+
+pdf.set_font(family="Arial", size=16)
+
+pdf.cell(w=0, h=12, txt="Hello there!", align="C", border=3)
+
+pdf.output("output.pdf")
