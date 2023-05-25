4b9266d7e60dc10faaf26075e528e5ff29fe0278
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-05-25 21:38:35 +0200

    Add another page

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ pdf.set_font(family="Arial", size=16)
 
 pdf.cell(w=0, h=12, txt="Hello there!", align="C", border=3)
 
+pdf.add_page()
+
+pdf.set_font(family="Times", size=14)
+
+pdf.cell(w=0, h=8, txt="Next page", align="L", border=1)
+
 pdf.output("output.pdf")
